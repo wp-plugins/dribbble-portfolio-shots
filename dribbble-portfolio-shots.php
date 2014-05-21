7354a41e7154bbdeffeb4c12d80503897b7c49e9
matt.king <matt.king@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-21 02:14:21 +0000

    dribbble portfolio shots version release
    git-svn-id: https://plugins.svn.wordpress.org/dribbble-portfolio-shots/trunk@918342 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dribbble-portfolio-shots.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package dribbble-portfolio-shots
+*/
+/*
+Plugin Name: Dribbble Portfolio Shots
+Plugin URI: http://www.ramit-designs.com
+Description: Dribbble Portfolio Shots - Showcase your best design work you shared on your dribbble profile on your website also.
+Version: 1.0
+Author: Matt Armstrong
+Author URI: http://www.ramit-designs.com
+*/
+// style sheets
+add_action( 'wp_enqueue_scripts', 'register_plugin_styles_dribbble_portfolio_shots_shortcode' );
+add_shortcode('dribbble_shots', 'dribbble_portfolio_shots_shortcode');
+
+function dribbble_portfolio_shots_shortcode($atts){
+    $atts = shortcode_atts(array(
+ 		'id' => 'dribbble',
+ 		'column' => '4' //column 2,4,6 available
+ 	), $atts);
+    extract($atts);
+    $data = "";
+    $data .= "
+        <div class='dribble_box'>
+            <div class='row center dribbble-shots'>
+            </div>
+        </div>
+";
+    
+ $data .= "
+<script>
+     var playerId = '$id';
+     var column = '$column';
+     get_dribbble(playerId,column);
+</script>
+";
+    return $data;
+    }
+
+ function register_plugin_styles_dribbble_portfolio_shots_shortcode() {
+     wp_register_style( 'dribbble_portfolio_shots_shortcode_flexboxgrid', plugins_url( 'assets/flexboxgrid.min.css' , __FILE__ ) );
+     wp_register_style( 'dribbble_portfolio_shots_shortcode_style', plugins_url( 'assets/style.css' , __FILE__ ) );
+     wp_register_script('dribbble_portfolio_shots_shortcode_jquery', '//code.jquery.com/jquery-latest.min.js');
+     wp_register_script('dribbble_portfolio_shots_shortcode_dribbblejs', plugins_url('assets/dribbble.js', __FILE__));
+     wp_enqueue_style( 'dribbble_portfolio_shots_shortcode_flexboxgrid' );
+     wp_enqueue_style( 'dribbble_portfolio_shots_shortcode_style' );
+     wp_enqueue_script('dribbble_portfolio_shots_shortcode_jquery');
+     wp_enqueue_script('dribbble_portfolio_shots_shortcode_dribbblejs');
+ }
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Dribbble Portfolio Shots ===
+
+Contributors: matt.king
+Tags: dribbble, dribbble portfolio shots, dribbble portfolio for wordpress, dribbble shortcode, wordpress shortcode
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Dribbble Portfolio Shots is a Wordpress Plugin for displaying shortcode.
+
+== Description ==
+
+Dribbble becoming popular day by day for designers and designers submit their portfolio items on it. We can understand the importance of Portfolio and Dribbble Portfolio Shots Shortcode will allow developer to put designer portfolio on their website.
+
+Few Features you will love
+
+*   Set default values to give you easy start
+*   Responsive in Nature
+*   Available 2 column, 4 column, 6 column grid
+*   Shortcode can be put on theme easily
+
+== Installation ==
+
+Installing this plugin is same like other wordpress plugins
+
+1. Upload `dribbble-portfolio-shots` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place <?php echo do_shortcode("[dribbble_shots]"); ?> in your themes
+
+You can install the zip file as well
+
+Available Shortcode
+
+1. Put your own dribbble username
+[dribbble_shots id="YOUR_DRIBBBLE_NAME"]
+
+2. for two column grid
+[dribbble_shots id="YOUR_DRIBBBLE_NAME" column=2]
+
+four column is default so doesn't need to mention
+[dribbble_shots id="YOUR_DRIBBBLE_NAME" column=4]
+
+Six column -
+[dribbble_shots id="YOUR_DRIBBBLE_NAME" column=6]
+
+More info -> http://ramit-designs.com/wordpress/
+
+== Frequently Asked Questions ==
+
+Is it free?
+Yap it is :)
+
+
+== Screenshots ==
+
+Demo available here - http://ramit-designs.com/wordpress/
+
+== Changelog ==
+
+= 1.0 =
+stable version release
